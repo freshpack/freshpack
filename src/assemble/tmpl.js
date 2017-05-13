39645d38ff2728ad4a06d3d5591d921c18016087
Uli Preuss <me@ulipreuss.eu>
2017-05-13 12:07:25 +0200

    renamed folder

--- src/assemble/tmpl.js
+++ src/assemble/tmpl.js
@@ -1,10 +1,10 @@
-const base = require('../options/base');
-const sass = require('../options/sass');
-const styled = require('../options/styled');
-const lint = require('../options/lint');
-const test = require('../options/test');
-const redux = require('../options/redux');
-const flow = require('../options/flow');
+const base = require('../components/base');
+const sass = require('../components/sass');
+const styled = require('../components/styled');
+const lint = require('../components/lint');
+const test = require('../components/test');
+const redux = require('../components/redux');
+const flow = require('../components/flow');
 
 module.exports = Object.assign(
  base, sass, lint, test, redux, flow, styled
